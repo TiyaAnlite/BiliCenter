3854ca292a4b8c6cc31b656d9637469a7405ca7a
TiyaAnlite <chenquanyu1999@126.com>
2021-02-24 00:47:09 +0800

    一些规范化

--- service/callback/bangumi.py
+++ service/callback/bangumi.py
@@ -11,19 +11,21 @@ def meta(callback: dict, r: redis.StrictRedis, sql_queue: queue.Queue, logger: l
     if callback["code"] == 200:
         mid = callback["data"]["media"]["media_id"]
         sid = callback["data"]["media"]["season_id"]
-        query = "INSERT IGNORE INTO `status_bangumi` (mid,sid) VALUES (%s,%s)"
-        args = [mid, sid]
-        sql_queue.put((query, args))
+        bangumi_meta = {
+            "mid": mid,
+            "sid": sid
+        }
+        sql_queue.put(make_insert_query("status_bangumi", bangumi_meta, safety_mode=True, update=True))
 
-        update_data = {
+        bangumi_data = {
             "title": callback["data"]["media"]["title"],
             "s_index": callback["data"]["media"]["new_ep"]["index"],
             "index_show": callback["data"]["media"]["new_ep"]["index_show"]
         }
         if "rating" in callback["data"]["media"]:
-            update_data["score_people"] = callback["data"]["media"]["rating"]["count"]
-            update_data["score"] = callback["data"]["media"]["rating"]["score"]
-        sql_queue.put(make_update_query("status_bangumi", update_data, dict(mid=mid)))
+            bangumi_data["score_people"] = callback["data"]["media"]["rating"]["count"]
+            bangumi_data["score"] = callback["data"]["media"]["rating"]["score"]
+        sql_queue.put(make_update_query("status_bangumi", bangumi_data, dict(mid=mid)))
         logger.info(f"Update bangumi mid->sid: {mid}->{sid}")
         # 生成事件InteractData, CollectiveInfo
         event_interact = new_event(SCFJobs.bangumi_interact_data, dict(season_id=sid), Sources.CallbackCenter)
@@ -37,26 +39,26 @@ def meta(callback: dict, r: redis.StrictRedis, sql_queue: queue.Queue, logger: l
 def interact(callback: dict, r: redis.StrictRedis, sql_queue: queue.Queue, logger: logging.Logger):
     if callback["code"] == 200:
         sid = callback["job"]["data"]["kwargs"]["season_id"]
-        update_data = {
+        interact_data = {
             "follow": callback["data"]["follow"],
             "series_follow": callback["data"]["series_follow"],
             "views": callback["data"]["views"],
             "danmakus": callback["data"]["danmakus"],
             "timestamp": int(time.time())
         }
-        sql_queue.put(make_update_query("status_bangumi", update_data, dict(sid=sid)))
+        sql_queue.put(make_update_query("status_bangumi", interact_data, dict(sid=sid)))
         logger.info(f"Update interact at {sid}")
 
 
 def collective(callback: dict, r: redis.StrictRedis, sql_queue: queue.Queue, logger: logging.Logger):
     if callback["code"] == 200:
         sid = callback["job"]["data"]["kwargs"]["season_id"]
-        update_data = {
+        collective_data = {
             "is_started": callback["data"]["publish"]["is_started"],
             "is_finish": callback["data"]["publish"]["is_finish"],
             "timestamp": int(time.time())
         }
-        sql_queue.put(make_update_query("status_bangumi", update_data, dict(sid=sid)))
+        sql_queue.put(make_update_query("status_bangumi", collective_data, dict(sid=sid)))
         logger.info(f"Update collective at {sid}, {len(callback['data']['episodes'])} videos")
         for ep in callback["data"]["episodes"]:
             ep_map = {
