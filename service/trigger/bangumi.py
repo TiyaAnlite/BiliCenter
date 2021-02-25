7c36fd01dca778f526a2772e0f7ddbe559d6ac2f
TiyaAnlite <chenquanyu1999@126.com>
2021-02-25 23:11:14 +0800

    完成bangumi相关trigger，番剧数据中心功能基本完成

---
+++ service/trigger/bangumi.py
@@ -0,0 +1,19 @@
+import logging
+
+import redis
+
+from bilicenter_middleware.event import new_event, SCFJobs, Sources
+
+
+def on_meta(rules_data: list, r: redis.StrictRedis, logger: logging.Logger):
+    # rules per rows: [mid]
+    data = rules_data[0]
+    logger.info(f"[Bangumi] Updating {len(data)} bangumi")
+    for row in data:
+        e = new_event(SCFJobs.bangumi_meta, dict(media_id=row[0]), Sources.TriggerEvent)
+        e.push(r)
+
+
+TRIGGER_INFO = {
+    "bangumi": on_meta
+}
